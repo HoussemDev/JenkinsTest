7d9c5dc88b49ddfec3411fcfcd4718893c1b119f
HoussemDEv <houss.sword@gmail.com>
2020-04-10 21:22:58 +0200

    firefox driver added

--- src/test/java/UrlTest.java
+++ src/test/java/UrlTest.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 public class UrlTest {
@@ -7,9 +9,18 @@ public class UrlTest {
 
     @Test
     public void openUrl( ) {
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver");
-        driver = new ChromeDriver();
+        System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/drivers/geckodriver");
+//        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver");
+//        driver = new ChromeDriver();
+        driver = new FirefoxDriver();
         driver.navigate().to("https://www.google.com/");
+    }
+
+
+
+    @AfterMethod
+    public void closeDriver( ) {
+     driver.close();
     }
 
 
