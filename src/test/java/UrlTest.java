ea3aa08fb2289fc6190f18af5246a93f40a52fd6
HoussemDev <houss.sword@gmail.com>
2020-04-14 15:50:53 +0200

    headless

--- src/test/java/UrlTest.java
+++ src/test/java/UrlTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
@@ -10,21 +11,24 @@ public class UrlTest {
 
     @Test
     public void openUrl( ) {
-        System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/drivers/geckodriver");
-//        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver");
+//        System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/drivers/geckodriver");
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver");
 //        driver = new ChromeDriver();
-        driver = new FirefoxDriver();
-
+//        driver = new FirefoxDriver();
+        ChromeOptions op = new ChromeOptions();
+        op.addArguments("--headless");
+        op.addArguments("--window-size=1920,1080");
+        driver = new ChromeDriver(op);
        driver.navigate().to("https://www.google.com/");
-        driver.quit();
+
     }
 
 
 
-//    @AfterMethod
-//    public void closeDriver( ) {
-//     driver.quit();
-//    }
+    @AfterMethod
+    public void closeDriver( ) {
+     driver.quit();
+    }
 
 
 }
